3408b3df3eb7f5317cfad0f5fde9b4ef45f32a2c
Joshua Aguilar <jaguilarl11@miumg.edu.gt>
2022-02-07 23:38:28 -0600

    tarea

---
+++ prueba (1).sql
@@ -0,0 +1,92 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 08-02-2022 a las 06:36:13
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `prueba`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `aplicaciones`
+--
+
+CREATE TABLE `aplicaciones` (
+  `aplcodigo` varchar(5) NOT NULL,
+  `aplnombre` varchar(60) NOT NULL,
+  `aplestado` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `bitacora`
+--
+
+CREATE TABLE `bitacora` (
+  `bitcodigo` int(11) NOT NULL,
+  `bitfecha` datetime NOT NULL,
+  `bitaccion` varchar(10) NOT NULL,
+  `usucodigo` varchar(15) NOT NULL,
+  `aplcodigo` varchar(5) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `perfil`
+--
+
+CREATE TABLE `perfil` (
+  `IDperfil` varchar(15) NOT NULL,
+  `Nombreperfil` varchar(30) NOT NULL,
+  `Estatusperfil` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `perfilusuario`
+--
+
+CREATE TABLE `perfilusuario` (
+  `IDperfil` varchar(15) NOT NULL,
+  `codigousuario` varchar(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `usucodigo` varchar(15) NOT NULL,
+  `usunombre` varchar(60) NOT NULL,
+  `usudireccion` varchar(60) NOT NULL,
+  `usutelefono` varchar(30) NOT NULL,
+  `usucorreo` varchar(60) NOT NULL,
+  `usucontra` varchar(100) NOT NULL,
+  `usuestado` varchar(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
